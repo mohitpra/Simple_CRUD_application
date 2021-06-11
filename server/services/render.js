f7135f6e78afdb1a6f3786ef87ad2664bb43a5ee
Mohit <ymohit2142@gmail.com>
2021-06-12 00:54:22 +0530

    All ejs file setups to render the updated data

--- server/services/render.js
+++ server/services/render.js
@@ -1,5 +1,16 @@
+const axios = require('axios');
+
+
+
 exports.homeRoutes = (req, res) => {
-    res.render("index");
+     axios.get('http://localhost:3000/api/users')
+            .then(response => {
+                res.render('index', {users: response.data});
+            })
+            .catch(err => {
+                res.send(err);
+            })
+    
 }
 
 exports.add_user = (req, res) => {
@@ -7,5 +18,13 @@ exports.add_user = (req, res) => {
 }
 
 exports.update_user = (req, res) => {
-    res.render("update_user")
+
+    axios.get('http://localhost:3000/api/users', {params: {id: req.query.id}})
+        .then(userData => {
+            res.render("update_user", {user: userData.data})
+        })
+        .catch(err => {
+            res.send(err);
+        })
+
 }
